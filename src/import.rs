694c44c9fcfd39df99879f05e1950f767fd7ce79
Marcus Ball <marcus.ball@live.com>
2019-10-16 14:41:29 -0400

    parse dates for entries

--- src/import.rs
+++ src/import.rs
@@ -33,3 +33,92 @@ impl RawEntry {
         })
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct Date {
+    pub day: String,
+    pub time: String,
+    pub context: Vec<String>,
+}
+
+impl Date {
+    pub fn from_entry(entry: &RawEntry, previous: &Date) -> Date {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r#"^(?P<day>(?:\d{1,2}\s\w{3})|(?:\w{3}\s\d{1,2}))?[,; ]*(?:(?P<context2>[^\r\n;,]*?)[;,]?)?(?:(?P<context1>[^\r\n;,]*?)[;,]?)?$"#
+            )
+            .unwrap();
+
+            static ref TIMES: Vec<&'static str> = vec!["morning", "afternoon", "evening", "night"];
+            static ref BRUNCH: String = String::from("brunch");
+        }
+        if entry.date.is_none() {
+            return previous.clone();
+        }
+
+        // Evaluate the regex and find any captures
+        let captures = RE.captures(entry.date.as_ref().unwrap()).unwrap();
+
+        // Helper function to retrieve matches by name, as an Option<String>
+        let cap_str = |name| {
+            captures
+                .name(name)
+                .map(|m| m.as_str().trim())
+                .filter(|s| *s != "")
+                .map(|s| s.to_lowercase())
+        };
+
+        let day = cap_str("day").unwrap_or(previous.day.clone());
+        let context1 = cap_str("context1");
+        let context2 = cap_str("context2");
+
+        let is_time_string = |context: Option<&String>| {
+            context
+                .map(|c| TIMES.contains(&c.as_ref()))
+                .unwrap_or(false)
+        };
+
+        // I frequently just write "brunch"; if so we'll mark this as "afternoon".
+        let is_brunch =
+            context1.contains(&BRUNCH as &String) || context2.contains(&BRUNCH as &String);
+
+        let time = match (
+            is_time_string(context1.as_ref()),
+            is_time_string(context2.as_ref()),
+        ) {
+            // If one of either is a time specifier, then use that value.
+            (true, false) => context1.clone().unwrap(),
+            (false, true) => context2.clone().unwrap(),
+            // If neither specify the time perioud, first check if "brunch" was present.
+            (false, false) => match is_brunch {
+                // If it was, then use "afternoon"
+                true => String::from("afternoon"),
+                // Otherwise, if this record is the same day as the previous,
+                // then continue using the same time as the previous.
+                // Use "night" otherwise.
+                false => match day == previous.day {
+                    true => previous.time.clone(),
+                    false => String::from("night"),
+                },
+            },
+            // There should be no case of "afternoon, night" etc.
+            (true, true) => panic!(
+                "Found two time strings, {} and {}!",
+                context1.unwrap(),
+                context2.unwrap()
+            ),
+        };
+
+        let context = vec![context1, context2]
+            .iter()
+            .filter(|c| c.is_some() && !c.contains(&time))
+            .map(|c| c.as_ref().unwrap().to_string())
+            .collect();
+
+        Date {
+            day: day,
+            time: time,
+            context: context,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(option_result_contains)]
+
 #[macro_use]
 extern crate lazy_static;
 
@@ -7,7 +9,7 @@ use std::io::BufReader;
 
 mod import;
 
-use import::RawEntry;
+use import::{Date, RawEntry};
 
 fn main() -> std::io::Result<()> {
     let f = File::open("drinks.csv")?;
@@ -15,20 +17,37 @@ fn main() -> std::io::Result<()> {
 
     let mut line = String::new();
 
+    let mut previous_date = Date {
+        day: "1 jan".into(),
+        time: "".into(),
+        context: vec![],
+    };
+
     while reader.read_line(&mut line)? > 0 {
         let entry = RawEntry::from_line(&line.trim());
 
-        match entry {
-            Some(entry) => println!(
-                "{:16} | {:10} | {:40} | {:5} | {:10}",
-                entry.date.unwrap_or("".into()),
-                entry.quantity.unwrap_or("?".into()),
-                entry.name.unwrap_or("####".into()),
-                entry.abv.unwrap_or("?".into()),
-                entry.volume.unwrap_or("?".into())
-            ),
-            None => println!("ERROR: Failed to parse '{}'", line),
-        }
+        let entry = match entry {
+            Some(e) => e,
+            None => {
+                println!("ERROR: Failed to parse '{}'", line);
+                line.clear();
+                continue;
+            }
+        };
+
+        let date = Date::from_entry(&entry, &previous_date);
+        previous_date = date.clone();
+
+        println!(
+            "{:6} | {:9} | {:10} | {:10} | {:40} | {:5} | {:10}",
+            date.day,
+            date.time,
+            date.context.join(", "),
+            entry.quantity.unwrap_or("?".into()),
+            entry.name.unwrap_or("####".into()),
+            entry.abv.unwrap_or("?".into()),
+            entry.volume.unwrap_or("?".into())
+        );
 
         line.clear();
     }
